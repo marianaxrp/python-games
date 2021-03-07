870f7efc87d08e5ab926681c65095c31aee78df7
Mariana <marianaxrp@gmail.com>
2021-03-07 01:12:29 -0300

    added a new game and separated the games into different files

---
+++ forca.py
@@ -0,0 +1,10 @@
+def jogar():
+    print("**********************************")
+    print("***Bem vindos ao jogo de forca!***")
+    print("**********************************")
+
+    print("Fim de jogo!")
+
+
+if(__name__ == "__main__"):
+    jogar()
