04e467cea7605cf48bb49957fcddc3a682a0ba2e
Mariana <marianaxrp@gmail.com>
2021-03-10 23:10:19 -0300

    implemented loop to ask for a guess and compare it to the secret word

--- forca.py
+++ forca.py
@@ -9,6 +9,16 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
         print("jogando")
 
     print("Fim de jogo!")
